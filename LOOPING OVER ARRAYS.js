c82a5a4bc1178f37311439bad1e51f0b41b33c84
Tapomoy Das <160303114+TAPOMOY1994@users.noreply.github.com>
2024-04-01 12:30:25 +0530

    Add files via upload

---
+++ LOOPING OVER ARRAYS.js
@@ -0,0 +1,14 @@
+const days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+for(i=0;i<days.length;i++)
+
+{
+    console.log(i,days[i]);
+}
+
+
+for(i=days.length-1;i>=0;i--)
+
+{
+    console.log(i,days[i]);
+}
